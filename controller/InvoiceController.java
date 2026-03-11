eb84418541a896a103613ab09ae39ac114c93c68
bharathkumar1904 <bharathkumarnaidu8143@gmail.com>
2026-03-11 10:22:13 +0530

    Implement InvoiceController with add and view methods

---
+++ controller/InvoiceController.java
@@ -0,0 +1,31 @@
+package com.klef.fsad.exam.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.klef.fsad.exam.model.Invoice;
+import com.klef.fsad.exam.service.InvoiceService;
+
+@RestController
+@RequestMapping("/invoice")
+public class InvoiceController 
+{
+    @Autowired
+    private InvoiceService invoiceService;
+
+    // POST - Add Invoice
+    @PostMapping("/add")
+    public Invoice addInvoice(@RequestBody Invoice invoice)
+    {
+        return invoiceService.addInvoice(invoice);
+    }
+
+    // GET - View All Invoices
+    @GetMapping("/viewall")
+    public List<Invoice> getInvoices()
+    {
+        return invoiceService.getAllInvoices();
+    }
+}
